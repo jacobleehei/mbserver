cd72ac55c5677f46eeb65bbe87e0f25722e02a5a
Jacob Lee <jacob.lee@winpromise.com>
2024-10-22 17:36:20 +0800

    add function readFullRTUPacket for ensuring the received data is completed

--- servertu.go
+++ servertu.go
@@ -26,7 +26,7 @@ func (s *Server) ListenRTU(serialConfig *serial.Config) (err error) {
 }
 
 func (s *Server) acceptSerialRequests(port serial.Port) {
-	SkipFrameError:
+SkipFrameError:
 	for {
 		select {
 		case <-s.portsCloseChan:
@@ -34,9 +34,7 @@ func (s *Server) acceptSerialRequests(port serial.Port) {
 		default:
 		}
 
-		buffer := make([]byte, 512)
-
-		bytesRead, err := port.Read(buffer)
+		packet, err := readFullRTUPacket(port)
 		if err != nil {
 			if err != io.EOF {
 				log.Printf("serial read error %v\n", err)
@@ -44,24 +42,40 @@ func (s *Server) acceptSerialRequests(port serial.Port) {
 			return
 		}
 
-		if bytesRead != 0 {
-
-			// Set the length of the packet to the number of read bytes.
-			packet := buffer[:bytesRead]
-
-			frame, err := NewRTUFrame(packet)
-			if err != nil {
-				log.Printf("bad serial frame error %v\n", err)
-				//The next line prevents RTU server from exiting when it receives a bad frame. Simply discard the erroneous 
-				//frame and wait for next frame by jumping back to the beginning of the 'for' loop.
-				log.Printf("Keep the RTU server running!!\n")
-				continue SkipFrameError
-				//return
-			}
-
-			request := &Request{port, frame}
-
-			s.requestChan <- request
+		frame, err := NewRTUFrame(packet)
+		if err != nil {
+			log.Printf("bad serial frame error %v\n", err)
+			//The next line prevents RTU server from exiting when it receives a bad frame. Simply discard the erroneous
+			//frame and wait for next frame by jumping back to the beginning of the 'for' loop.
+			log.Printf("Keep the RTU server running!!\n")
+			continue SkipFrameError
+			//return
 		}
+
+		request := &Request{port, frame}
+
+		s.requestChan <- request
+	}
+}
+
+func readFullRTUPacket(port serial.Port) ([]byte, error) {
+	packet := make([]byte, 0)
+	for {
+		if len(packet) >= 3 {
+			packetLength := packet[2] + 5 // including header, slave address, function code and CRC
+			if len(packet) >= int(packetLength) {
+				return packet[:packetLength], nil
+			}
+		}
+		tmpBuf := make([]byte, 1)
+		_, err := port.Read(tmpBuf)
+		if err != nil {
+			if err != io.EOF {
+				log.Printf("serial read error %v\n", err)
+			}
+			return packet, err
+		}
+
+		packet = append(packet, tmpBuf...)
 	}
 }
